fa40e541458c29c101b967e3c52336b069d40b8b
Panfeng Zhang <panfeng.zhang@blackboard.com>
2016-02-21 15:26:09 +0800

    test

---
+++ src/com/darren/pattern/adapter/AdapterTest.java
@@ -0,0 +1,10 @@
+package com.darren.pattern.adapter;
+
+public class AdapterTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("test");
+	}
+
+}
